95da918af3c078b96b1244011525c6134e8afa7c
Matchima 025 <nee_bdsl@hotmail.com>
2018-07-06 15:22:01 +0700

    Update infomation

--- javascript/custom.js
+++ javascript/custom.js
@@ -156,7 +156,7 @@
         function init() {
             var mapOptions = {
                 zoom: 17,
-                center: new google.maps.LatLng(51.5287352, -0.3817831),
+                center: new google.maps.LatLng(6.847438, 99.756230),
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 scrollwheel: false,
                 disableDefaultUI: false
